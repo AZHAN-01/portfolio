d5ac0f12fbcc53b2793a4cd42b05a3c624e9040b
AZHAN <mohdazhannajar@gmail.com>
2026-06-14 02:15:12 +0530

    debug env variables

--- server.js
+++ server.js
@@ -55,6 +55,14 @@ console.log("DB_USER =", process.env.DB_USER);
 console.log("DB_PORT =", process.env.DB_PORT);
 console.log("DB_NAME =", process.env.DB_NAME);
 console.log("DB_PASSWORD exists =", !!process.env.DB_PASSWORD);
+console.log("ALL DB VARS:");
+console.log({
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PORT: process.env.DB_PORT,
+  DB_NAME: process.env.DB_NAME,
+  DB_PASSWORD_EXISTS: !!process.env.DB_PASSWORD
+});
     pool = mysql.createPool({
       ...dbConfig,
       waitForConnections: true,
