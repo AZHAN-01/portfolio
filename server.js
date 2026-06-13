1a5ac68e913310a5a10caedfdbc953e07efea9c5
AZHAN <mohdazhannajar@gmail.com>
2026-06-14 01:26:24 +0530

    Fix Railway MySQL connection

--- server.js
+++ server.js
@@ -50,7 +50,11 @@ async function initDB() {
       port: Number(process.env.DB_PORT),
       database: process.env.DB_NAME
     };
-
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_USER =", process.env.DB_USER);
+console.log("DB_PORT =", process.env.DB_PORT);
+console.log("DB_NAME =", process.env.DB_NAME);
+console.log("DB_PASSWORD exists =", !!process.env.DB_PASSWORD);
     pool = mysql.createPool({
       ...dbConfig,
       waitForConnections: true,
